1eb3f9eae702821970524870bc112ca76720b271
EMG114 <egutierrez411@gmail.com>
2017-07-23 15:24:06 -0400

    "UItextField doesn't accept whitespace or special characters now"

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -14,18 +14,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        self.nameField.delegate = self
-       self.lyricsView.text = String()
+        nameField.delegate = self
+        lyricsView.text = String()
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+  
     func shortNameFromName(name: String) -> String {
+        
         
         let lowercaseName = name.lowercased()
         let vowelSet = CharacterSet(charactersIn: "aeiou")
@@ -40,16 +38,22 @@ class ViewController: UIViewController {
     
     func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
         
-        let shortName = shortNameFromName(name: fullName)
+     
+        
+        
+       let shortName = shortNameFromName(name: fullName)
         
         let lyrics = lyricsTemplate
             .replacingOccurrences(of: "<FULL_NAME>", with: fullName)
             .replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
         
+        
         self.lyricsView.text = lyrics
         
+      
         return lyrics
     }
+   
     
     
     @IBAction func reset(_ sender: Any) {
@@ -59,11 +63,32 @@ class ViewController: UIViewController {
 
     @IBAction func displayLyrics(_ sender: Any) {
         
-        let sillyLyrics = "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME> \n" + "Banana Fana Fo F<SHORT_NAME> \n" + "Me My Mo M<SHORT_NAME> \n" + "<FULL_NAME>"
         
-       print(lyricsForName(lyricsTemplate: sillyLyrics, fullName: nameField.text!))
+        guard let fullName = nameField.text, fullName.characters.count > 0 else {
+            lyricsView.text = ""
+            let alertController = UIAlertController(title: "No Text", message: "Please Enter Valid Text In The Box", preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.default)
+            {
+                (result : UIAlertAction) -> Void in
+                print("You pressed OK")
+            }
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+
+            return
+        }
+      
+        let sillyLyrics = "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME> \n" + "Banana Fana Fo F<SHORT_NAME> \n" + "Me My Mo M<SHORT_NAME> \n" + "<FULL_NAME>"
+      
+        
+       print(lyricsForName(lyricsTemplate: sillyLyrics, fullName: (nameField.text)!))
+        
+     
+        
 
     }
+    
+   
 
 }
 
@@ -73,5 +98,15 @@ extension ViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         return false
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+    let characterSet = CharacterSet.letters
+    
+    if string.rangeOfCharacter(from: characterSet.inverted) != nil {
+    return false
+    }
+    return true
+}
 }
 
